3332b04cb7d8cb3211e610d12f4a007aaacb8172
Rodrigar <rodriadriangarcia1997@gmail.com>
2022-11-29 04:29:37 -0300

    productos terminados

--- src/componentes/CardWidget.js
+++ src/componentes/CardWidget.js
@@ -2,7 +2,7 @@
 
 
 import '../componentes/CardWidget.css'
-
+import ItemListContainer from './ItemsListContainer';
 
 const CardWidget = () =>{
     return( 
@@ -20,7 +20,7 @@ const CardWidget = () =>{
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div className="modal-body">
-       Tus productos
+       <ItemListContainer props={"Tus Productos"}/>
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
---
+++ src/componentes/ItemsListContainer.js
@@ -0,0 +1,14 @@
+
+
+const ItemListContainer = ({props}) =>{
+    return(
+        <div>
+            <h2>{props}</h2>
+        </div>
+
+
+    )
+
+}
+
+export default ItemListContainer
